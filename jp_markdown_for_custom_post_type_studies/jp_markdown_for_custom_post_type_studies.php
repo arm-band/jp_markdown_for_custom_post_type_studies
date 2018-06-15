453776a8060f9f054283117a516eec3c90a5aa64
Arm=Band <bre4tn.let@gmail.com>
2018-06-15 16:56:26 +0900

    first commit.

---
+++ jp_markdown_for_custom_post_type_studies/jp_markdown_for_custom_post_type_studies.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: JP Markdown for Custom Post Type Studies
+Description: 投稿タイプ「考察」でJP MarkdownによるMarkdownパースを有効にする
+Version: 0.0.2
+Author: アルム＝バンド
+*/
+add_action('init', 'studies_init');
+function studies_init() {
+	add_post_type_support('studies', 'wpcom-markdown');
+}
